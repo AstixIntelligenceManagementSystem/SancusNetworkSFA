382d9dcf66c53034ef424585055bfe0cae0689c2
SUNIL-PC <astixset2@gmail.com>
2018-07-02 17:57:05 +0530

    update by sunil

---
+++ app/src/main/java/com/astix/Common/Discussion.java
@@ -0,0 +1,23 @@
+package com.astix.Common;
+
+public class Discussion
+{
+
+   // Store Mapping, remove validation--->Done
+   // Remove Select DSR in Store Mapping,also remove heading--->Done
+    // Add Store functionality--->
+   // DSR tracker remove--->Done
+   // sales Tareget remove--->Done
+  //  Purchase Order remove--->Done
+
+   // No Alert in Market Visit--->direct Progress bar and Store Selection will come--->Done
+
+  //  Store Checkin Page will added--->Done
+  //  editBox will come on product page--->Done
+  //  add new Page for Display page,submit button on Order page,not go to Store Selection,a new page will be added-->Done
+
+
+    // As per Avinash Sir, Distributor Stock not come in All Button Activity on 28 june 2018
+
+
+}
